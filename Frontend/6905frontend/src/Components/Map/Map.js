d676e5159d4fde9d2fa6a7533950a34e4fa6670d
keyvan booshehri <keyvan@keyvans-mbp.home>
2024-03-17 05:26:48 -0230

    Map updated

--- Frontend/6905frontend/src/Components/Map/Map.js
+++ Frontend/6905frontend/src/Components/Map/Map.js
@@ -1,15 +1,26 @@
 import "./Map.css";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { locationIcon } from "./icons";
 import { useState } from "react";
 
 const Map = () => {
+  const routeColor = { color: "orange" };
   const [locations, setLocation] = useState([
-    [48.94831625381342, -56.73100121642103],
-    [49.94831625381342, -56.83100121642103],
-    [49.74831625381342, -56.83100121642103],
-    [48.54831625381342, -57.83100121642103],
+    [48.9494835325974, -57.83091895065246],
+    [48.94682720740517, -57.82500746120248],
+    [48.94818944311722, -57.835897047031395],
+    [48.945737392059314, -57.830400398946324],
+    [48.94260404035938, -57.83050410928755],
+    [48.93654112743416, -57.83019297826386],
+  ]);
+  const [polyline, setPolyline] = useState([
+    [48.9494835325974, -57.83091895065246],
+    [48.94682720740517, -57.82500746120248],
+    [48.94818944311722, -57.835897047031395],
+    [48.945737392059314, -57.830400398946324],
+    [48.94260404035938, -57.83050410928755],
+    [48.93654112743416, -57.83019297826386],
   ]);
   return (
     <div className="map-container">
@@ -25,6 +36,7 @@ const Map = () => {
         {locations.map((location) => {
           return <Marker icon={locationIcon} position={location} />;
         })}
+        <Polyline pathOptions={routeColor} positions={polyline} />
         {/* <Marker
           icon={locationIcon}
           position={[48.94831625381342, -57.83100121642103]}
