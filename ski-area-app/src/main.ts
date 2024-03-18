fec8c2ce115884fc0b570578200106eae6bd7d5e
Shuvo Barman <20889458+Shuvob4@users.noreply.github.com>
2024-03-18 01:22:54 -0230

    Added frontend 3028 port for accessing backend

--- ski-area-app/src/main.ts
+++ ski-area-app/src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  /*
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  */
+  const app = await NestFactory.create(AppModule);
+app.enableCors({
+  origin: 'http://localhost:3000', // Allow only your frontend to access the backend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify HTTP methods allowed
+  allowedHeaders: 'Content-Type, Accept', // Specify HTTP headers allowed
+});
+await app.listen(3028);
+
 }
 bootstrap();
