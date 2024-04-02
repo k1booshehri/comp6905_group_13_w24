58a3cc01f7c15b9c4158c3083f5e8e721e8c1e2a
Shuvo Barman <20889458+Shuvob4@users.noreply.github.com>
2024-04-01 22:02:13 -0230

    Category Logic implemented

--- ski-area-app/src/route/models/graph.model.ts
+++ ski-area-app/src/route/models/graph.model.ts
@@ -3,7 +3,7 @@ import { Edge, Level, LiftType, isLift } from './edge.model';
 export class Graph {
   public nodes: Set<string> = new Set();
   public adjList: Map<string, Edge[]> = new Map();
-  
+
   public addNode(node: string): void {
     this.nodes.add(node);
     this.adjList.set(node, []);
@@ -39,7 +39,7 @@ export class Graph {
     });
     return allEdges;
   }
-  
+
   public getAllEdgesWithOverview(): { nodes: string[], levels: Level[], types: LiftType[], edges: Edge[] } {
     const levelsSet: Set<Level> = new Set();
     const typesSet: Set<LiftType> = new Set();
@@ -48,17 +48,17 @@ export class Graph {
     // Adjusted scope for these arrays
     const nodesArray = Array.from(this.nodes);
     const allEdges = this.getAllEdges().filter(edge => {
-        // For lifts, check and include only one direction
-        if (isLift(edge)) {
-            if (uniqueLifts.has(edge.name)) {
-                return false; // This lift (in one direction) is already included
-            }
-            uniqueLifts.add(edge.name); // Mark this lift as included
+      // For lifts, check and include only one direction
+      if (isLift(edge)) {
+        if (uniqueLifts.has(edge.name)) {
+          return false; // This lift (in one direction) is already included
         }
-        // Collect levels and types
-        if (edge.level) levelsSet.add(edge.level);
-        if (edge.type) typesSet.add(edge.type);
-        return true; // Include this edge
+        uniqueLifts.add(edge.name); // Mark this lift as included
+      }
+      // Collect levels and types
+      if (edge.level) levelsSet.add(edge.level);
+      if (edge.type) typesSet.add(edge.type);
+      return true; // Include this edge
     });
 
     // Now `levelsArray` and `typesArray` can directly use the sets filled during filtering
@@ -71,68 +71,62 @@ export class Graph {
   public getGraphOverview(): { nodes: string[], levels: Level[], edges: Edge[] } {
     // Initialize a Set to store all unique levels
     const levelsSet: Set<Level> = new Set();
-  
+
     // Iterate over all edges to extract unique levels
     this.adjList.forEach(edges => {
       edges.forEach(edge => {
         levelsSet.add(edge.level);
       });
     });
-  
+
     // Convert the nodes Set and levels Set to arrays for JSON compatibility
     const nodesArray = Array.from(this.nodes);
     const levelsArray = Array.from(levelsSet);
-  
+
     // Also, let's get all the edges for completeness
     const allEdges = this.getAllEdges();
-  
+
     // Return an object containing nodes, levels, and all edges
     return { nodes: nodesArray, levels: levelsArray, edges: allEdges };
   }
 
-  
+
   public findAllRoutes(start: string, end: string, level: Level): { message: string, routes: any[] } {
     let routes = [];
     const visited = new Set<string>();
     const uniquePaths = new Set<string>(); // To store unique paths
-  
-    const dfs = (node: string, path: Edge[]) => {
+
+    const dfs = (node: string, path: Edge[], liftCount = 0) => {
       if (node === end) {
-        const { totalTime, totalDistance, categories } = this.calculatePathDetails(path, level);
+        const { totalTime, totalDistance, categories, finalDistance } = this.calculatePathDetails(path, level);
         const pathKey = path.map(edge => edge.name).join('-'); // Generating a unique key for the path
         if (!uniquePaths.has(pathKey)) { // Check if path is unique
-          routes.push({ path: [...path], totalTime, totalDistance, categories });
+          routes.push({ path: [...path], totalTime, totalDistance, categories, finalDistance, liftCount });
           uniquePaths.add(pathKey); // Add path key to set
         }
         return;
       }
-  
+
       visited.add(node);
       const edges = this.adjList.get(node) || [];
-  
+
       for (const edge of edges) {
         if (!visited.has(edge.end) && (edge.level === level || isLift(edge))) {
-          // Apply level filtering for slopes only; include all lifts regardless of their level
-          dfs(edge.end, [...path, edge]);
+          dfs(edge.end, [...path, edge], liftCount + (isLift(edge) ? 1 : 0));
         }
       }
-  
+
       visited.delete(node);
     };
-  
+
     dfs(start, []);
-  
-    if (routes.length === 0) { // Check if no routes are found
-      return { message: "No path found with given conditions", routes: [] };
+
+    if (routes.length === 0) {
+      return { message: "No path found with given conditions, Please select another level and check. Or Change starting or Ending Point.", routes: [] };
     }
-  
-    // Here, directly incorporate a basic categorization logic before returning the results
-    // For simplicity, this example does not include a separate categorizeRoutes method
-    routes.forEach(route => {
-      // Simple example of categorization logic, adjust according to your needs
-      route.category = route.totalDistance > 1000 ? "Long route" : "Short route";
-    });
-  
+
+    this.categorizeAndCompareRoutes(routes);
+
     return {
       message: "Routes Overview",
       routes: routes.map(route => ({
@@ -148,39 +142,58 @@ export class Graph {
         })),
         totalTime: route.totalTime,
         totalDistance: route.totalDistance,
+        finalDistance: route.finalDistance,
         categories: route.categories,
-        category: route.category // Include the direct category assignment here
+        liftCount: route.liftCount 
       }))
-    };  
+    };
   }
-  
-  private calculatePathDetails(path: Edge[], requestedLevel: Level) {
+
+
+  private calculatePathDetails(path: Edge[], requestedLevel: Level): { totalTime: number, totalDistance: number, categories: string[], finalDistance: number, liftCount: number } {
     let totalTime = 0;
     let totalDistance = 0;
-    let categories = []; // Initialize categories array
-  
-    // Calculate totalTime and totalDistance as before
+    let liftCount = 0;
+    let difficultyFactor = 0;
+
     for (const edge of path) {
+      totalTime += edge.time;
+      totalDistance += edge.distance;
       if (isLift(edge)) {
-          // If it's a lift, add its time to totalTime
-          totalTime += edge.time;
-          totalDistance += edge.distance;
+        liftCount += 1; 
       } else {
-          // If it's a slope, calculate time based on distance and add to totalTime
-          // and also add its distance to totalDistance
-          totalTime += edge.distance / 840; // Slope time calculation: distance / 840 to convert to minutes
-          totalDistance += edge.distance;
+        difficultyFactor += edge.level === 'Easy' ? 0.5 : edge.level === 'Intermediate' ? 1 : 1.5;
       }
     }
-  
-    // Example categorization based on totalDistance (you can adjust this logic)
-    if (totalDistance > 2000) {
-      categories.push("Long");
-    } else {
-      categories.push("Short");
+
+    const finalDistance = totalDistance * difficultyFactor;
+
+    return { totalTime, totalDistance, categories: [], finalDistance, liftCount };
+  }
+
+  private categorizeAndCompareRoutes(routes: any[]) {
+    // Initialize variables to find the extremes
+    let minTime = Infinity, maxDistance = 0, minLiftCount = Infinity;
+    let minFinalDistance = Infinity, maxFinalDistance = 0;
+
+    // First pass to find the extreme values
+    for (const route of routes) {
+      minTime = Math.min(minTime, route.totalTime);
+      maxDistance = Math.max(maxDistance, route.totalDistance);
+      minLiftCount = Math.min(minLiftCount, route.liftCount);
+      minFinalDistance = Math.min(minFinalDistance, route.finalDistance);
+      maxFinalDistance = Math.max(maxFinalDistance, route.finalDistance);
     }
-  
-    return { totalTime, totalDistance, categories };
+
+    // Second pass to assign categories based on the extremes
+    routes.forEach(route => {
+      route.categories = [];
+      if (route.finalDistance === minFinalDistance) route.categories.push('Easiest');
+      if (route.finalDistance === maxFinalDistance) route.categories.push('Hardest');
+      if (route.totalDistance === maxDistance) route.categories.push('Longest');
+      if (route.totalTime === minTime) route.categories.push('Fastest');
+      if (route.liftCount === minLiftCount) route.categories.push('Minimum Lift Usage');
+    });
   }
 
 }
